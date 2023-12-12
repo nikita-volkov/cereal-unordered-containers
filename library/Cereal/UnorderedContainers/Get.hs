d8ed063981227a92045993fd9d44173d77b3aeb1
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-12-12 17:36:25 +0300

    Modernize

--- library/Cereal/UnorderedContainers/Get.hs
+++ library/Cereal/UnorderedContainers/Get.hs
@@ -1,11 +1,12 @@
-module Cereal.UnorderedContainers.Get
-where
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
-import Cereal.UnorderedContainers.Prelude
+module Cereal.UnorderedContainers.Get where
+
 import qualified Cereal.UnorderedContainers.Extras.StrictHashMap as StrictHashMap
-
+import Cereal.UnorderedContainers.Prelude
 
 strictHashMap :: (Eq key, Hashable key) => Get Int -> Get key -> Get value -> Get (StrictHashMap.HashMap key value)
-strictHashMap int key value = size >>= associations where
-  size = int
-  associations size = StrictHashMap.replicateM size ((,) <$> key <*> value)
+strictHashMap int key value = size >>= associations
+  where
+    size = int
+    associations size = StrictHashMap.replicateM size ((,) <$> key <*> value)
